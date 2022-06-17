a3d899cbb11c18d8f1b3326b5c0068a4070fe536
Bayuxz <107552948+Bayuxz@users.noreply.github.com>
2022-06-18 06:09:33 +0700

    Update config.js

--- src/drips/config.js
+++ src/drips/config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','447441437150']
-global.premium = ['27634090203','447441437150']
-global.botname = 'ZIM BOT INC'
-global.ownername ='DRIPS'
-global.packname = 'ZIM BOT INC'
-global.quoted = 'https://www.youtube.com/c/DRIPSOFC'
-global.monayawal = '100000'
+global.owner = ['089506312533']
+global.premium = ['-']
+global.botname = 'SyinXD'
+global.ownername ='BayXZ'
+global.packname = 'SyinXD'
+global.quoted = 'https://youtube.com/channel/UCvR7ud3DmDhFfQtn15gr4nQ'
+global.monayawal = '20000'
 global.darahawal = '100'
 global.besiAwal = '15'
 global.goldAwal = '10'
@@ -57,7 +57,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'SyinXD'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
